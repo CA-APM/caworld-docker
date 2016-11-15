8189d9366a16c0ce3fd4d55bea9a581ecb037535
grogu01 <Guenter.Grossberger@ca.com>
2016-11-15 10:52:55 -0800

    added jmeter load script

---
+++ baseline.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+
+SCRIPT=mathapp-baseline
+SCRIPT_DIR=.
+LOG_DIR=.
+
+JMETER_DIR=./apache-jmeter-3.0
+
+if [ ! -e $JMETER_DIR ] ; then
+    wget http://apache.claz.org/jmeter/binaries/apache-jmeter-3.0.tgz
+    tar -xvzf apache-jmeter-3.0.tgz
+fi
+
+#${JMETER_DIR}/bin/jmeter.sh -n -t ${SCRIPT_DIR}/${SCRIPT}.jmx -j ${LOG_DIR}/jmeter-${SCRIPT}.log 1>/dev/null &
+# -l ${LOG_DIR}/${SCRIPT}.jtl
